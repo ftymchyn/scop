e0a2b0a133fc5d80acafa9d45ebe20fe80578556
ftymchyn <fazzan.07@gmail.com>
2019-05-15 08:30:33 +0300

    added stub .obj dropfile parsing

--- srcs/events/handle_dropfile.c
+++ srcs/events/handle_dropfile.c
@@ -1,12 +1,31 @@
 #include "scop.h"
 
+static void		clear_face(void *data)
+{
+	t_vector	*vec;
+
+	vec = *(t_vector**)data;
+	vector_clear( &vec, NULL );
+}
+
 int	handle_dropfile(void *scop, SDL_Event *e)
 {
 	if (scop && e)
 	{
 		if (e->type == SDL_DROPFILE)
 		{
-			ft_putendl(e->drop.file);
+			if (ft_strequ(ft_strrchr(e->drop.file, '.'), ".obj"))
+			{
+				t_obj o = parse_obj(e->drop.file);
+
+				vector_clear( &o.v, NULL );
+				vector_clear( &o.vn, NULL );
+				vector_clear( &o.vt, NULL );
+				vector_clear( &o.f, &clear_face );
+			}
+			else
+				ft_putendl("Can parse only *.obj");
+			
 			SDL_free(e->drop.file);
 		}
 	}
