b4b95a931cf00baa985153dd16169e1ebc7c1c63
ftymchyn <fazzan.07@gmail.com>
2019-06-09 14:17:15 +0300

    updated dropfile handling

--- srcs/events/handle_dropfile.c
+++ srcs/events/handle_dropfile.c
@@ -2,14 +2,19 @@
 
 int	handle_dropfile(void *scop, SDL_Event *e)
 {
+	t_scop *s;
+	t_darr models;
+
 	if (scop && e)
 	{
+		s = (t_scop*)scop;
 		if (e->type == SDL_DROPFILE)
 		{
 			if (ft_strequ(ft_strrchr(e->drop.file, '.'), ".obj"))
 			{
-				t_darr models = load_models(e->drop.file);
-				clear_models(&models);
+				models = load_models(e->drop.file);
+				clear_models(&s->scene.models);
+				s->scene.models = models;
 			}
 			else
 				ft_putendl("Can parse only *.obj");
