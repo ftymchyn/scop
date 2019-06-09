ff9700554c2221ec8a51559f7955bb1739f8b32a
ftymchyn <fazzan.07@gmail.com>
2019-06-09 14:17:37 +0300

    implemented render_model

--- srcs/loop.c
+++ srcs/loop.c
@@ -1,54 +1,36 @@
 #include "scop.h"
 
-void	loop(t_scop *scop)
+static void	render_model(void *data)
+{
+	t_model	*model;
+	t_mesh	*mesh;
+	size_t	i;
+
+	model = (t_model*)data;
+	i = 0;
+	while (i < model->meshes.size)
+	{
+		mesh = (t_mesh*)darr_at(&model->meshes, i);
+		glBindVertexArray(mesh->vao);
+		glDrawArrays(GL_TRIANGLES, 0, mesh->v.size);
+		glBindVertexArray(0);
+		i++;
+	}
+}
+
+void		loop(t_scop *scop)
 {
 	scop->running = TRUE;
-	GLfloat buffer[] = {
-		-0.5f, -0.5f,  1.0f, 0.1f, 0.1f,
-		 0.5f, -0.5f,  0.1f, 1.0f, 0.1f,
-		 0.5f,  0.5f,  1.0f, 0.1f, 0.1f,
-		-0.5f,  0.5f,  0.1f, 0.1f, 1.0f
-	};
-	GLuint indices[] = {
-		0, 1, 2,
-		2, 3, 0
-	};
-
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	GLuint vbo;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(buffer), buffer, GL_STATIC_DRAW);
-	GLuint ibo;
-	glGenBuffers(1, &ibo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (GLvoid*)0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (GLvoid*)(sizeof(float) * 2));
-	glBindVertexArray(0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	GLuint program = create_shader_program( "srcs/shaders/simple.shader");
+	GLuint program = create_shader_program( "srcs/shaders/model.glsl");
 
 	while (scop->running)
 	{
 		SDL_PumpEvents();
-
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glBindVertexArray(vao);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glUseProgram(program);
-		
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint), GL_UNSIGNED_INT, NULL);
-
-		glBindVertexArray(0);
+		darr_foreach(&scop->scene.models, &render_model);
 		glUseProgram(0);
-
 		SDL_GL_SwapWindow(scop->window);
 	}
+	clear_models(&scop->scene.models);
 }
