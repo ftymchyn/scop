c68c4d8f190cafa45c84c45061a64de1860fff45
ftymchyn <fazzan.07@gmail.com>
2019-06-15 17:39:15 +0300

    added camera computations to camera init

--- srcs/scene/init_scene.c
+++ srcs/scene/init_scene.c
@@ -2,11 +2,18 @@
 
 void	init_camera(t_camera *cam, float width, float height)
 {
+	float	fov_y;
+	float	aspect;
+
+	fov_y = 45.0f;
+	aspect = width / height;
 	cam->width = width;
 	cam->height = height;
-	cam->fov = 45.0f;
-	cam->ar = cam->width / cam->height;
 	cam->pos = (t_float3){ 0.0f, 0.0f, 3.0f };
+	cam->near = 0.1f;
+	cam->far = 100.0f;
+	cam->rt_point.y = tanf(fov_y * 0.5 * 3.14159 / 180) * cam->near;
+	cam->rt_point.x = cam->rt_point.y * aspect;
 }
 
 void	init_scene(t_scene *scene)
