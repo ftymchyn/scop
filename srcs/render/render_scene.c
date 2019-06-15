ef4a1d109437c6e308670c23071622c90725c14a
ftymchyn <fazzan.07@gmail.com>
2019-06-15 18:48:04 +0300

    added and inited uniforms for MVP

--- srcs/render/render_scene.c
+++ srcs/render/render_scene.c
@@ -1,9 +1,22 @@
 #include "scop.h"
 
+void	prepare_mat4_uniforms(GLuint program, t_scene *s)
+{
+	GLint u_location;
+
+	u_location = glGetUniformLocation(program, "model");
+	glUniformMatrix4fv(u_location, 1, GL_TRUE, s->m_model.d);
+	u_location = glGetUniformLocation(program, "view");
+	glUniformMatrix4fv(u_location, 1, GL_TRUE, s->m_view.d);
+	u_location = glGetUniformLocation(program, "proj");
+	glUniformMatrix4fv(u_location, 1, GL_TRUE, s->m_proj.d);
+}
+
 void	render_scene(t_scene *scene)
 {
 	GL_CALL(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 	GL_CALL(glUseProgram(scene->model_shader));
+	prepare_mat4_uniforms(scene->model_shader, scene);
 	render_model(&scene->models);
 	GL_CALL(glUseProgram(0));
 }
--- srcs/shaders/model.glsl
+++ srcs/shaders/model.glsl
@@ -7,11 +7,15 @@ layout(location = 2) in vec2 a_uv;
 
 out vec3 frag_color;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
+
 void main()
 {
-	float intensity = max(dot(a_normal, vec3(0.0, 0.0, -1.0)),0.0);
-	frag_color = vec3(1.0) * intensity;
-	gl_Position = vec4(a_position, 1.0);
+	float intensity = max(dot(a_normal, vec3(0.0, 0.0, 1.0)),0.0);
+	frag_color = vec3(0.7) * intensity;
+	gl_Position = proj * view * model * vec4(a_position, 1.0);
 }
 
 //FRAGMENT SHADER
