d0750b5ef8b31d3169be996c781ce24ea61617eb
ftymchyn <fazzan.07@gmail.com>
2019-06-15 19:20:18 +0300

    m_lookat replaced by m_view

--- srcs/math/m_view.c
+++ srcs/math/m_view.c
@@ -1,10 +1,9 @@
 #include "scop.h"
 
-t_mat4		m_lookat(t_float3 pos, t_camera *cam)
+t_mat4		m_view(t_camera *cam)
 {
 	t_mat4		result;
 
-	(void)pos;
 	m_identity(&result);
 	result.d[11] = cam->pos.z * -1.0f;
 	return (result);
