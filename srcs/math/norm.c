b6a563b1cd7412e4f47ae1aa69e4b1896fe3d61f
ftymchyn <fazzan.07@gmail.com>
2019-06-17 16:39:55 +0300

    added zero checking, added norm4f

--- srcs/math/norm.c
+++ srcs/math/norm.c
@@ -6,6 +6,17 @@ t_float3	norm3f(t_float3 v)
 	float	len;
 
 	len = sqrtf(dot3f(v, v));
-	len = 1.0f / len;
+	if (len > 0.0f)
+		len = 1.0f / len;
+	return (v * len);
+}
+
+t_float4	norm4f(t_float4 v)
+{
+	float	len;
+
+	len = sqrtf(dot4f(v, v));
+	if (len > 0.0f)
+		len = 1.0f / len;
 	return (v * len);
 }
