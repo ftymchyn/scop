b479969754df15aa21a68752cbce41baa9cb244a
ftymchyn <fazzan.07@gmail.com>
2019-06-18 23:18:44 +0300

    refactoring rendering, added skybox rendering

--- srcs/render/render_model.c
+++ srcs/render/render_model.c
@@ -1,25 +1,39 @@
 #include "scop.h"
 
-void	render_model(t_darr *models)
+static void	draw_model(void *data)
 {
 	t_model	*model;
 	t_mesh	*mesh;
 	size_t	i;
-	size_t	k;
 
+	model = (t_model*)data;
 	i = 0;
-	while (i < models->size)
+	while (i < model->meshes.size)
 	{
-		model = (t_model*)darr_at(models, i);
-		k = 0;
-		while (k < model->meshes.size)
-		{
-			mesh = (t_mesh*)darr_at(&model->meshes, k);
-			glBindVertexArray(mesh->vao);
-			glDrawArrays(GL_TRIANGLES, 0, mesh->v.size);
-			glBindVertexArray(0);
-			k++;
-		}
+		mesh = (t_mesh*)darr_at(&model->meshes, i);
+		glBindVertexArray(mesh->vao);
+		glDrawArrays(GL_TRIANGLES, 0, mesh->v.size);
+		glBindVertexArray(0);
 		i++;
 	}
+}
+
+static void	update_model_shader(t_scene *s)
+{
+	GLint u_location;
+
+	u_location = glGetUniformLocation(s->model_shader, "model");
+	glUniformMatrix4fv(u_location, 1, GL_TRUE, s->m_model.d);
+	u_location = glGetUniformLocation(s->model_shader, "view");
+	glUniformMatrix4fv(u_location, 1, GL_TRUE, s->m_view.d);
+	u_location = glGetUniformLocation(s->model_shader, "proj");
+	glUniformMatrix4fv(u_location, 1, GL_TRUE, s->m_proj.d);
+}
+
+void		render_models(t_scene *scene)
+{
+	GL_CALL(glUseProgram(scene->model_shader));
+	update_model_shader(scene);
+	darr_foreach(&scene->models, &draw_model);
+	GL_CALL(glUseProgram(0));
 }
---
+++ srcs/render/render_skybox.c
@@ -0,0 +1,23 @@
+#include "scop.h"
+
+static void	update_skybox_shader(t_scene *s)
+{
+	GLint u_location;
+
+	u_location = glGetUniformLocation(s->skybox.shader, "model");
+	glUniformMatrix4fv(u_location, 1, GL_TRUE, s->m_model.d);
+}
+
+void	render_skybox(t_scene *scene)
+{
+	GL_CALL(glUseProgram(scene->skybox.shader));
+	update_skybox_shader(scene);
+	GL_CALL(glBindVertexArray(scene->skybox.vao));
+	GL_CALL(glActiveTexture(GL_TEXTURE0));
+	GL_CALL(glBindTexture(GL_TEXTURE_CUBE_MAP, scene->skybox.cubemap));
+	GL_CALL(glDepthFunc(GL_LEQUAL));
+	GL_CALL(glDrawArrays(GL_TRIANGLES, 0, 6));
+	GL_CALL(glDepthFunc(GL_LESS));
+	GL_CALL(glBindVertexArray(0));
+	GL_CALL(glUseProgram(0));
+}
