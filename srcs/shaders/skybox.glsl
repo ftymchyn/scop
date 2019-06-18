b0d73a6a7a429ee529e345581d1281f933182b69
ftymchyn <fazzan.07@gmail.com>
2019-06-18 23:19:09 +0300

    added skybox shader

---
+++ srcs/shaders/skybox.glsl
@@ -0,0 +1,27 @@
+//VERTEX SHADER
+#version 330 core
+
+layout(location = 0) in vec2 a_position;
+layout(location = 0) in vec2 a_texCoord;
+
+out vec3 texDir;
+
+uniform mat4 model;
+
+void main()
+{
+	texDir = mat3(model) * vec3(a_texCoord, 1.0);
+	gl_Position = vec4(a_position, 1.0, 1.0);
+}
+
+//FRAGMENT SHADER
+#version 330 core
+
+in vec3 texDir;
+
+uniform samplerCube skyboxCubemap;
+
+void main()
+{
+	gl_FragColor = texture(skyboxCubemap, texDir);
+}
