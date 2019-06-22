2757e0fc2ee9dba46aa15b1bd6bf1dd6196c068a
ftymchyn <fazzan.07@gmail.com>
2019-06-22 14:10:48 +0300

    updated view matrix

--- srcs/math/m_view.c
+++ srcs/math/m_view.c
@@ -5,6 +5,8 @@ t_mat4		m_view(t_camera *cam)
 	t_mat4		result;
 
 	m_identity(&result);
-	result.d[11] = cam->pos.z * -1.0f;
+	result.d[3] = cam->pos.x * -1.0f;
+	result.d[7] = cam->pos.y * -1.0f;
+	result.d[11] = (cam->pos.z + cam->near) * -1.0f;
 	return (result);
 }
