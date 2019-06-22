fdcba0eede9678c555fb445b6d0a6889410ab268
ftymchyn <fazzan.07@gmail.com>
2019-06-22 14:12:40 +0300

    set camera position in init

--- srcs/scene/init_scene.c
+++ srcs/scene/init_scene.c
@@ -9,7 +9,7 @@ void	init_camera(t_camera *cam, float width, float height)
 	aspect = width / height;
 	cam->width = width;
 	cam->height = height;
-	cam->pos = (t_float3){ 0.0f, 0.0f, 3.0f };
+	cam->pos = (t_float3){ 0.0f, 0.0f, 1.0f };
 	cam->near = 1.0f;
 	cam->far = 100.0f;
 	cam->rt_point.y = tanf(fov_y * 0.5 * 3.14159f / 180.0f) * cam->near;
