1df196d8f3b288cff60062bc9dcbd9416f217cee
ftymchyn <fazzan.07@gmail.com>
2019-06-27 08:23:57 +0300

    handle switching ortho to persp projection event

--- srcs/events/handle_keyboard.c
+++ srcs/events/handle_keyboard.c
@@ -2,11 +2,21 @@
 
 int	handle_keyboard(void *scop, SDL_Event *e)
 {
+	t_events	*events;
+
 	if (scop && e)
 	{
-		if (e->type == SDL_KEYUP && e->key.keysym.sym == SDLK_ESCAPE)
+		events = &((t_scop*)scop)->events;
+		if (e->type == SDL_KEYUP)
 		{
-			((t_scop*)scop)->events.should_close = TRUE;
+			if (e->key.keysym.sym == SDLK_ESCAPE)
+				events->should_close = TRUE;
+			else if (e->key.keysym.sym == SDLK_KP_5)
+			{
+				events->is_ortho_projection = !events->is_ortho_projection;
+				((t_scop*)scop)->scene.camera.pos.z +=
+					(events->is_ortho_projection ? 1.0f : -1.0f);
+			}
 		}
 	}
 	return (1);
