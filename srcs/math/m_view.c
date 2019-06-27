0b23ee53831b738b98b26f23a1c44de49c3f876c
ftymchyn <fazzan.07@gmail.com>
2019-06-27 08:21:44 +0300

    fixed view

--- srcs/math/m_view.c
+++ srcs/math/m_view.c
@@ -7,6 +7,6 @@ t_mat4		m_view(t_camera *cam)
 	result = m_rotmatrix_quat(cam->q_rotation);
 	result.d[3] = cam->pos.x * -1.0f;
 	result.d[7] = cam->pos.y * -1.0f;
-	result.d[11] = (cam->pos.z + cam->near) * -1.0f;
+	result.d[11] = cam->pos.z * -1.0f;
 	return (result);
 }
